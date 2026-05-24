cfc5c8eee06d8a6ad20b69c6cdc017af7a2808fd
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-05-24 22:40:49 +0200

    feat: scrapped tracks and their data

--- collect_data.py
+++ collect_data.py
@@ -5,17 +5,14 @@ from dotenv import load_dotenv
 # import os
 from rich import print
 import json
+import time
+import pandas as pd
 
 load_dotenv()
 with open("data/artists.json", "r") as f:
     ARTISTS = json.load(f)
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
-
-# artist = sp.search(q="Taco Hemingway", type="artist", limit=1)
-
-# `15 character`
-# artist["artists"]["items"][0]["uri"][15:]
 
 
 def get_artist_albums(artist_id):
@@ -25,7 +22,43 @@ def get_artist_albums(artist_id):
     return albums
 
 
-artist_albums = get_artist_albums(ARTISTS["Taco Hemingway"])
+def create_artist_data(artist_albums):
+    tracks = {}
+    tracks_list = []
 
-for i in range(len(artist_albums)):
-    print(artist_albums[i]["id"], artist_albums[i]["name"])
+    for i in range(len(artist_albums)):
+        tracks[artist_albums[i]["name"]] = sp.album_tracks(
+            artist_albums[i]["id"], limit=15
+        )
+
+        # print(
+        #     [
+        #         track["duration_ms"] / 60000
+        #         for track in tracks[artist_albums[i]["name"]]["items"]
+        #     ]
+        # )
+
+        for track in tracks[artist_albums[i]["name"]]["items"]:
+            tracks_list.append(
+                {
+                    "name": track["name"],
+                    "duration_ms": track["duration_ms"] / 60000,
+                    "explicit": track["explicit"],
+                    "track_number": track["track_number"],
+                    "album": artist_albums[i]["name"],
+                }
+            )
+
+        return tracks_list
+
+
+tracks_list = []
+
+# for artist in ARTISTS:
+#     artist_albums = get_artist_albums(artist_id=ARTISTS[artist])
+#     time.sleep(2)
+#     tracks_list.extend(create_artist_data(artist_albums=artist_albums))
+#     time.sleep(2)
+
+df = pd.DataFrame(tracks_list)
+df.to_csv("data/tracks.csv", index=False)
